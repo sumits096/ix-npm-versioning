ff5825cf5a1f498bf73a2b9c6d7d798e5c1f5c3f
Sumit <sumitshinde096@gmail.com>
2022-02-23 12:33:23 +0530

    changes for deployment

---
+++ src/core/services/customer.service.ts
@@ -0,0 +1,7 @@
+import { CustomerInterface } from '../../api/interfaces/customer.interface';
+
+export class CustomerService implements CustomerInterface {
+  getAll(): string[] {
+    throw new Error('Method not implemented 2.');
+  }
+}
