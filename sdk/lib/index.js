6d880d76d7faac19a9b643dc15b081b8e492a460
Sumit <sumitshinde096@gmail.com>
2022-03-07 15:12:08 +0530

    refactor: updated readme.md

--- sdk/lib/index.js
+++ sdk/lib/index.js
@@ -2,9 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Credential = exports.BoomtownProviders = void 0;
 require("reflect-metadata");
+var controllers_1 = require("./api/controllers");
+var services_1 = require("./core/services");
+var boomtown_client_1 = require("./gateway/client/boomtown-client");
+var repositories_1 = require("./gateway/repositories");
+var request_handler_1 = require("./gateway/request-handler");
 var BoomtownProviders = function (token, secret) {
     new Credential().setTokenSecret(token, secret);
-    return [];
+    return [
+        { provide: controllers_1.Customer, deps: [services_1.CustomerService] },
+        { provide: services_1.CustomerService, deps: [repositories_1.CustomerRepository] },
+        { provide: repositories_1.CustomerRepository, deps: [boomtown_client_1.BoomtownClient] },
+        { provide: boomtown_client_1.BoomtownClient, deps: [request_handler_1.AxiosHandler] },
+    ];
 };
 exports.BoomtownProviders = BoomtownProviders;
 var Credential = (function () {
